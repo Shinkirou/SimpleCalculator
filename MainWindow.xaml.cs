9eb1741f75f89975c20594181f4233cc5743ec19
Shinkirou <shinkirou20230101@gmail.com>
2023-04-17 14:43:45 +0800

    1.0存檔
    1.介面設置
    2.按鍵綁定
    3.1~9輸入數字程式碼

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,55 @@ namespace SimpleCalculator
         {
             InitializeComponent();
         }
+        ///1到9按鍵綁定
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = txtNumber.Text + "0";
+        }
+
+        private void btnOne_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = txtNumber.Text + "1";
+        }
+
+        private void btnTwo_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = txtNumber.Text + "2";
+        }
+
+        private void btnThree_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = txtNumber.Text + "3";
+        }
+
+        private void btnFour_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = txtNumber.Text + "4";
+        }
+
+        private void btnFive_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = txtNumber.Text + "5";
+        }
+
+        private void btnSix_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = txtNumber.Text + "6";
+        }
+
+        private void btnSeven_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = txtNumber.Text + "7";
+        }
+
+        private void btnEight_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = txtNumber.Text + "8";
+        }
+
+        private void btnNine_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = txtNumber.Text + "";
+        }
     }
 }
