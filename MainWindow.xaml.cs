d282de75738b54c34b1e98cad2cb450eb98e0577
Shinkirou <shinkirou20230101@gmail.com>
2023-04-17 15:56:07 +0800

    2.0存檔
    1.= 按鍵綁定
    2.加減乘除按鍵綁定
    3.1到9事件綁定修正
    4.AC事件綁定修正
    5.全域變數設定
    6.小數點事件綁定

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,12 +25,79 @@ namespace SimpleCalculator
             InitializeComponent();
         }
 
+        float firstNumber, secondNumber; // firstNumber 儲存第一個數字，secondNumber 儲存第二個數字
+        int operators = -1; // 記錄選擇哪一種運算符號？0:加、1:減、2:乘、3:除、-1:重新設定
+
+        /// AC(清除按鍵)事件綁定
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
-            txtNumber.Text = " ";
+            txtNumber.Text = "0";
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
         }
 
-        ///1到9按鍵綁定
+        ///加減乘除事件綁定
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第一個數字的全域變數
+            txtNumber.Text = "0"; //重新將輸入文字框重新設定為0
+            operators = 0; //選擇「加」號
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 1; //選擇「減」號
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 2; //選擇「乘」號
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 3; //選擇「除」號
+        }
+
+        /// = 事件綁定
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            float finalResults = 0f; //宣告最後計算結果變數
+            secondNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第二個數字的全域變數
+
+            //依照四則運算符號的選擇，進行加減乘除
+            switch (operators)
+            {
+                case 0:
+                    finalResults = firstNumber + secondNumber;
+                    break;
+                case 1:
+                    finalResults = firstNumber - secondNumber;
+                    break;
+                case 2:
+                    finalResults = firstNumber * secondNumber;
+                    break;
+                case 3:
+                    finalResults = firstNumber / secondNumber;
+                    break;
+            }
+
+            txtNumber.Text = string.Format("{0:0.##########}", finalResults); //在輸入文字框中，顯示最後計算結果，並且轉換成格式化的字串內容
+
+            //重置所有全域變數
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
+        }
+
+        ///1到9事件綁定
         private void btnZero_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
@@ -58,7 +125,7 @@ namespace SimpleCalculator
         }
 
 
-            private void btnThree_Click(object sender, RoutedEventArgs e)
+        private void btnThree_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
             {
@@ -128,5 +195,12 @@ namespace SimpleCalculator
             txtNumber.Text = txtNumber.Text + "9";      
         }
 
+        ///小數點事件綁定
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+            // 確認輸入文字框中完全沒有小數點
+            if (txtNumber.Text.IndexOf(".") == -1)
+                txtNumber.Text = txtNumber.Text + ".";
+        }
     }
 }
